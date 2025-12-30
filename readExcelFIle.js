a78c4973946c4721e3e003d59c121b3b0cb9b12d
unknown <melika78karimi@gmail.com>
2025-12-30 10:31:34 +0330

    add second way for read excel

--- readExcelFIle.js
+++ readExcelFIle.js
@@ -1,12 +1,25 @@
 import exceljs from "exceljs";
 // way1
 
-const newWorkbook = new exceljs.Workbook();
-newWorkbook.xlsx.readFile("download.xlsx").then(() => {
+// const newWorkbook = new exceljs.Workbook();
+// newWorkbook.xlsx.readFile("download.xlsx").then(() => {
+//   const workSheet = newWorkbook.getWorksheet("Sheet1");
+//   workSheet.eachRow((row, rowNumber) => {
+//     row.eachCell((cell, colNumber) => {
+//       console.log(cell.value);
+//     });
+//   });
+// });
+
+// way2
+async function excelTest() {
+  const newWorkbook = new exceljs.Workbook();
+  await newWorkbook.xlsx.readFile("download.xlsx");
   const workSheet = newWorkbook.getWorksheet("Sheet1");
-  workSheet.eachRow((row, rowNumber) => {
+  await workSheet.eachRow((row, rowNumber) => {
     row.eachCell((cell, colNumber) => {
       console.log(cell.value);
     });
   });
-});
+}
+excelTest();

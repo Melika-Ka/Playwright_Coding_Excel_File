facdc02e64577369ae5d4acdad08b4c99d64ffd5
unknown <melika78karimi@gmail.com>
2025-12-31 10:12:01 +0330

    How to get and update the data from excel based on filter search criteria

--- 3-UpdateExcelFIlewithFilter.js
+++ 3-UpdateExcelFIlewithFilter.js
@@ -1,20 +1,31 @@
 import exceljs from "exceljs";
 // way2
-let output = { row: -1, col: -1 };
-async function excelTest() {
+async function excelTest(searchText, replaceText, change, filePath) {
   const newWorkbook = new exceljs.Workbook();
-  await newWorkbook.xlsx.readFile("download.xlsx");
+  await newWorkbook.xlsx.readFile(filePath);
   const workSheet = newWorkbook.getWorksheet("Sheet1");
+  const output = await readExcel(workSheet, searchText);
+  const cell = workSheet.getCell(
+    output.row + change.rowChange,
+    output.col + change.colChange
+  );
+  cell.value = replaceText;
+  await newWorkbook.xlsx.writeFile("updatedFile.xlsx");
+  console.log(output);
+  console.log(searchText);
+  console.log(cell.value);
+}
+async function readExcel(workSheet, searchText) {
+  let output = { row: -1, col: -1 };
+
   workSheet.eachRow((row, rowNumber) => {
     row.eachCell((cell, colNumber) => {
-      if (cell.value == "Apple") {
+      if (cell.value == searchText) {
         output.row = rowNumber;
         output.col = colNumber;
       }
     });
   });
-  const cell = workSheet.getCell(output.row, output.col);
-  cell.value = "Iphone";
-  await newWorkbook.xlsx.writeFile("updatedFile.xlsx");
+  return output;
 }
-excelTest();
+excelTest("Mango", 350, { rowChange: 0, colChange: 2 }, "download.xlsx");
